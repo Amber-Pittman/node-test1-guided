b6aa82efc2cbad00f64cb8503303d04f1fb42b22
Amber-Pittman <amber@amberpittman.com>
2020-05-14 12:21:04 -0400

    Still have an error in Divide even though matches Lecture code

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -14,24 +14,46 @@ function add(...values) {
     return values.reduce((a, b) => a + b, 0)
 }
 
-function subtract(...values) {
-    return values.reduce((a, b) => a - b, 0)
+function subtract(a = 0, b = 0) {
+	return a - b
 }
+
+// function subtract(...values) {
+//     return values.reduce((a, b) => a - b, 0)
+// }
 
 function multiply(a = 0, b = 0) {
-    return values.reduce((a, b) => a * b, 0)
+	return a * b
 }
+// function multiply(...values) {
+//     return values.reduce((a, b) => a * b, 0)
+// }
+
 
 function divide(a = 1, b = 1) {
-    if (typeof a !== "number" || typeof b !== "number") {
+	// Typescript is useful for these exact situations, without
+	// having to constantly check the data types
+	if (typeof a !== "number" || typeof b !== "number") {
 		throw new TypeError("Expected a number, got something else")
 	}
-    
-    if (b === 0) {
-        throw new Error("Cannot divide by zero.")
-    }
-    return a / b
+
+	if (b === 0) {
+		throw new Error("Cannot divide by zero")
+	}
+	
+	return a / b
 }
+
+// function divide(a = 1, b = 1) {
+//     if (typeof a !== "number" || typeof b !== "number") {
+// 	 throw new TypeError("Expected a number, got something else")
+// 	}
+    
+//     if (b === 0) {
+//         throw new Error("Cannot divide by zero.")
+//     }
+//     return a / b
+// }
 
 module.exports = {
     add,
